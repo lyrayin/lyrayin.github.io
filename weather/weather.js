60096cd5fe021f006688a8cf10728e8f6731f2d4
lyrayin <35706908+lyrayin@users.noreply.github.com>
2021-04-09 21:48:06 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -21,7 +21,7 @@ $.getJSON(weatherAlertsUrl, function(data) {
     // Color all alert polygons orange, but color Severe polygons red
     style: function(feature){
       var alertColor = 'orange';
-      if (feature.properties.severity === 'Severe') alertColor = 'purple';
+      if (feature.properties.severity === 'Severe') alertColor = 'Magenta';
       return { color: alertColor }
     },
     // Add a popup on each feature showing the NWS alert headline
