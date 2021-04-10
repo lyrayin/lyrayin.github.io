bf2fdf69048fdb6a7a709dec8722d49b3a941597
lyrayin <35706908+lyrayin@users.noreply.github.com>
2021-04-09 21:47:17 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -22,8 +22,6 @@ $.getJSON(weatherAlertsUrl, function(data) {
     style: function(feature){
       var alertColor = 'orange';
       if (feature.properties.severity === 'Severe') alertColor = 'purple';
-      if (feature.properties.severity === 'Extreme') alertColor = 'purple';
-      if (feature.properties.severity === 'Minor') alertColor = 'blue';
       return { color: alertColor }
     },
     // Add a popup on each feature showing the NWS alert headline
